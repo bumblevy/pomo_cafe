2b05ae90d0e1bace72007dbc60774b2516e06ae1
bumblevy <ky.vy@berkeley.edu>
2022-08-05 18:06:52 -0700

    Found a sound to use as beep

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ class Pomodoro:
     A program that can help you keep track of time
 
     Instance Variables:
-        default_time
+        pomDuration: Duration of pomodoro timer in minutes
     """
 
-    def __init__(self, default_time):
-        self.default_time = 25
+    def __init__(self, pomDuration):
+        self.pomDuration = pomDuration
+        self.secondsInMin = 60
 
 #Ref: https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-python/
+# Sound Ref: https://freesound.org/people/Rudmer_Rotteveel/sounds/536422/
 
     def countdown(self, countdown_time):
         while countdown_time:
@@ -22,13 +24,23 @@ class Pomodoro:
             time.sleep(1)
             countdown_time -= 1
         print("Time is finished! (: ")
+        self.play_beep()
         # seconds = time.time()
         # print("Time in seconds since the epoch:", seconds)
         # local_time = time.ctime(seconds)
         # print("Local time: ", local_time)
+    
+    # Will start the timer and begin countodwn
+    def start_timer(self):
+        totalSecs = self.pomDuration * self.secondsInMin
+        self.countdown(totalSecs)
+    
+    def play_beep(self):
+        playsound('beep.wav')
 
 def main():
     limit = input("Enter time to countdown from: ")
-    pomodoro = Pomodoro(25)
-    pomodoro.countdown(int(limit))
+    pomodoro = Pomodoro(int(limit))
+    pomodoro.start_timer()
+    
 main()

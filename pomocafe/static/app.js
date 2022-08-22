d768e77ffd937182bfb2159cd556085eeead509e
bumblevy <ky.vy@berkeley.edu>
2022-08-21 23:18:14 -0700

    Implemented pomo button event

--- pomocafe/static/app.js
+++ pomocafe/static/app.js
@@ -1,34 +1,112 @@
-let number = document.getElementById('number')
-let counter = 0
-const startingMinutes = 1;
-let time = startingMinutes * 60;
+class Pomodoro {
+    constructor(startTime, stopTime) {
+        this.startTime = startTime;
+        this.stopTime = stopTime;
+    }
 
-
-const countdownEl = document.getElementById('number')
-// setInterval(updateCountdown, 1000);
-animateProgressBar(time);
-startTimer(time);
-
-function startTimer(duration) {
-    let start = Date.now(), diff, minutes, seconds;
-    timer();
-    let id = setInterval(timer, 1000);
-    function timer() {
-        diff = duration - (((Date.now() - start) / 1000) | 0);
-
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        countdownEl.innerHTML = `${minutes}: ${seconds}`;
-
-        if (diff <= 0) {
-            clearInterval(id);
+    /* Used to start the timer with the current time */
+    startTimer(duration) {
+        let start = Date.now(), diff, minutes, seconds;
+        timer();
+        let id = setInterval(timer, 1000);
+        function timer() {
+            diff = duration - (((Date.now() - start) / 1000) | 0);
+    
+            minutes = (diff / 60) | 0;
+            seconds = (diff % 60) | 0;
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    
+            if (diff <= 0) {
+                clearInterval(id);
+            }
         }
     }
+
+    /* Used when the player wants to stop the timer, it will pause timer */
+    stopTimer() {
+
+    }
+
+    /* Used to trigger short break option: 5 minutes */
+    shortBreak() {
+
+    }
+
+    /* Used to trigger long break option: 15 minutes */
+    longBreak() {
+
+    }
+
+    updateDisplay() {
+        animateProgressBar(this.startTime);
+    }
 }
+
+
+
+let number = document.getElementById('number')
+let counter = 0
+const startingMinutes = 25;
+let time = startingMinutes * 60;
+const countdownEl = document.getElementById('number')
+const pomodoro = new Pomodoro(time, 0)
+//pomodoro.startTimer(time)
+// pomodoro.updateDisplay()
+
+const pomodoroButton = document.querySelector('.button1')
+const shortButton = document.querySelector('.button2')
+const longButton = document.querySelector('.button3')
+const startButton = document.querySelector('.button4')
+const stopButton = document.querySelector('.button5')
+
+pomodoroButton.addEventListener('click', () => {
+    pomodoro.startTimer(time)
+    pomodoro.updateDisplay()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(updateCountdown, 1000);
+// animateProgressBar(time);
+// startTimer(time);
+
+// function startTimer(duration) {
+//     let start = Date.now(), diff, minutes, seconds;
+//     timer();
+//     let id = setInterval(timer, 1000);
+//     function timer() {
+//         diff = duration - (((Date.now() - start) / 1000) | 0);
+
+//         minutes = (diff / 60) | 0;
+//         seconds = (diff % 60) | 0;
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         countdownEl.innerHTML = `${minutes}: ${seconds}`;
+
+//         if (diff <= 0) {
+//             clearInterval(id);
+//         }
+//     }
+// }
+
+
 //     setInterval(function() {
 //         minutes = Math.floor(timer / 60);
 //         seconds = timer % 60;
